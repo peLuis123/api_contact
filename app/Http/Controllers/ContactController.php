eec8000ea48319057403450491f9b2afad1475ce
Pedro Luis Ramos Calla <pedro.ramos@devitm.com>
2023-07-19 17:53:20 -0500

    correcion ruta send-email

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -13,6 +13,14 @@ class ContactController extends Controller
             'email' => $request->input('email'),
             'message' => $request->input('message'),
         ];
+        if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+            return response()->json(['error' => 'Dirección de correo electrónico no válida', 'email' => $data['email']], 400);
+        }
+
+        // Verificar que el mensaje no esté vacío
+        if (empty($data['message'])) {
+            return response()->json(['error' => 'El mensaje no puede estar vacío'], 400);
+        }
         Mail::raw($data['message'], function ($message) use ($data) {
             $message->to('pedro.ramos@devitm.com')
                     ->subject('Nuevo mensaje de contacto')
